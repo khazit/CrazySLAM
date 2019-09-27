866d6a9c27915ed764475f2be700a58c606343da
Ali Benkassou <a.benkassou1@gmail.com>
2019-09-27 22:27:52 +0200

    [NOT TESTED] Add a proper abort command

--- fly_and_log.py
+++ fly_and_log.py
@@ -15,56 +15,94 @@ from cflib.positioning.motion_commander import MotionCommander
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger()
 
-VELOCITY = 0.2 # meters/second
-RATE = 360.0 / 5 # degrees/second
+VELOCITY = 0.1      # meters/second
+RATE = 360.0 / 5    # degrees/second
+YAW_DEG = 10        # degrees
 
 
 def connect():
-    """
-    """
+    """Find the adress of the first available Crazyflie"""
     cflib.crtp.init_drivers(enable_debug_driver=False)
     available = cflib.crtp.scan_interfaces()
-    assert len(available)!=0, "No Crazyflie found"
+    assert len(available) != 0, "No Crazyflie found"
     return available[0][0]
 
-def keyController(mc, velocity_x, velocity_y):
+
+def keyController(mc, current_velocity_x, current_velocity_y):
+    """Manage the keyboard control commands
+
+    Keymap:
+        z:      Forward
+        q:      Left
+        s:      Backward
+        d:      Right
+        a:      Yaw left
+        e:      Yaw right
+        DOWN:   Land (Crazyflie lands softly)
+        ENTER:  Abort (Shut down motors)
+
+    Each command increments the speed in the proper axis/direction by VELOCITY
+    and then starts a linear motion with the updated velocity values on both
+    x and y axis.
+    Yaw commands however are blocking: at each press, the Crazyflie yaw for
+    YAW_DEG degrees.
+
+    Args:
+        mc: MotionCommander object
+        current_velocity_x: Current velocity on the x axis
+        current_velocity_y: CUrrent velocity on the y axis
+
+    Returns:
+        bool: True if ABORT command received
+        bool: True if LAND command received
+        float: Velocity along the x direction
+        float: Velocity along the y direction
     """
-        z : forward
-        q : left
-        s : backward
-        d : right
-        a : yaw left
-        e : yaw right
-        down : abort
-    """
-    mc.stop()
     key = getkey()
-    if key == 'z':
-        velocity_x += VELOCITY
-    elif key == 'q':
-        velocity_y += VELOCITY
-    elif key == 's':
-        velocity_x -= VELOCITY
-    elif key == 'd':
-        velocit_y -= VELOCITY
-    elif key == 'a':
-        mc.start_turn_left(rate=RATE)
-    elif key == 'e':
-        mc.start_turn_right(rate=RATE)
+    # ABORT
+    if key == keys.ENTER:
+        return True, False, None, None
+    # OK LANDO, BOX BOX BOX
     elif key == keys.DOWN:
-        return True, 0, 0
-    return False, velocity_x, velocity_y
+        mc.stop()
+        return False, True, 0, 0
+    # Control commands
+    elif key == 'z':
+        current_velocity_x += VELOCITY
+    elif key == 'q':
+        current_velocity_y += VELOCITY
+    elif key == 's':
+        current_velocity_x -= VELOCITY
+    elif key == 'd':
+        current_velocity_y -= VELOCITY
+    elif key == 'a':
+        mc.turn_left(angle_degrees=YAW_DEG, rate=RATE)
+    elif key == 'e':
+        mc.turn_right(angle_degrees=YAW_DEG, rate=RATE)
+    # Start motion
+    mc.start_linear_motion(
+        current_velocity_x,
+        current_velocity_y,
+        0
+    )
+    return False, False, current_velocity_x, current_velocity_y
+
 
 if __name__ == '__main__':
     cf = Crazyflie(rw_cache="cache")
-    abort = False
+    abort, land = False
+    velocity_x = 0.0
+    velocity_y = 0.0
     with SyncCrazyflie(connect(), cf=cf) as scf:
         with MotionCommander(scf, default_height=1) as mc:
-            while not abort:
-                abort, velocity_x, velocit_y = keyController(mc)
-                motion_commander.start_linear_motion(
+            while not abort and not land:
+                abort, land, velocity_x, velocity_y = keyController(
+                    mc,
                     velocity_x,
-                    velocity_y,
-                    0
+                    velocity_y
                 )
+                # TODO: replace with log function?
+                print(velocity_x, velocity_y)
                 time.sleep(0.1)
+            if abort:
+                cf.send_stop_setpoint()
