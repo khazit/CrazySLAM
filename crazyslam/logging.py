78c540b68f92dbde314713dbf7ec0508073582f9
Ali Benkassou <a.benkassou1@gmail.com>
2019-11-17 15:58:05 +0100

    Add log file manager

--- crazyslam/logging.py
+++ crazyslam/logging.py
@@ -1,24 +1,27 @@
 """Logging module
 
 This module logs variables that are coming from the Crazyflie (range data and a
-state estimate (x, y and yaw)) that are needed for the slam algorithm. Also
+state estimate (x, y and yaw)) and that are needed for the slam algorithm. Also
 writes all the logged data to disk for post flight analysis
+
+PROBLEM: File name have to be hardcoded in callback function
 """
 
 import os
+import time
 from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
 
-def init_log_conf(scf, callback):
+def init_log_conf(scf, callback, data_dir):
     """Initialize and start the logging
 
     Args:
         scf: Synced Crazyflie
         callback: Function called when new data is received
-
+        data_dir: Directory where the log file will be saved
     """
-    log_conf = LogConfig(name='MainLog', period_in_ms=1000)
+    log_conf = LogConfig(name='MainLog', period_in_ms=100)
 
     # Logged variables
     # Multiranger
@@ -35,18 +38,48 @@ def init_log_conf(scf, callback):
 
     scf.cf.log.add_config(log_conf)
     log_conf.data_received_cb.add_callback(callback)
+    start_file_manager(data_dir)
     log_conf.start()
 
 
-def write_to_disk(dir, timestamp, data):
+def start_file_manager(data_dir):
+    """Log files manager
+
+    Make sure the data directory exists and create the blank log file with
+    right header
+
+    Args:
+        data_dir: Path to the log files directory
+    """
+    header = "timestamp,down,up,front,back,left,right,x,y,yaw\n"  # File header
+    epoch = int(time.time())
+    filename = "flight_{}.log".format(epoch)
+
+    # If directory doesn't exist, create one
+    if not os.path.isdir(data_dir):
+        os.mkdir(data_dir)
+    # Create sub directory for log files and figures
+    sub_dir = os.path.join(data_dir, "flight_{}".format(epoch))
+    os.mkdir(sub_dir)
+
+    # Write header
+    print("Log file :", filename)
+    f = open(os.path.join(sub_dir, filename), "a")
+    f.write(header)
+    f.close()
+
+
+def write_to_disk(file_path, timestamp, data):
     """Log data to disk"""
-    # TODO: create dir/file if they don't exist. File with header
-    assert os.path.isdir(dir), "No data directory found"
-    filename = "data"
+    assert os.path.isfile(file_path), "No log file found @ " + file_path
+
+    # Concatenate all fieds into one string
     data_point = str(timestamp)
     for key, value in data.items():
-        data_point += ", {}".format(value)
+        data_point += ",{}".format(value)
     data_point += "\n"
-    f = open(os.path.join(dir, filename), "a")
+
+    # Write data
+    f = open(file_path, "a")
     f.write(data_point)
     f.close()
--- examples/sim_logger.py
+++ examples/sim_logger.py
@@ -1,23 +1,28 @@
 import sys
+import time
+import logging
 from crazyslam.logging import *
+from cflib.crazyflie import Crazyflie
+from crazyslam.utils import get_address
 
 
 def logging_callback(timestamp, data, logconf):
     """Receive the data at each timestamp"""
-    print("New data received")
-    print(data)
-    write_to_disk("data", timestamp, data)
+    print("New data received", data)
+    epoch_id = os.listdir("log")[0].split("_")[1]
+    write_to_disk(
+        "log/flight_{}/flight_{}.log".format(epoch_id, epoch_id),  # TEMP
+        int(time.time()),
+        data
+    )
 
 
 if __name__ == '__main__':
-    from cflib.crazyflie import Crazyflie
-    from crazyslam.utils import get_address
-    import time
-    i = 0
+    start = int(time.time())
+    end = start + int(sys.argv[1])
     cf = Crazyflie(rw_cache="cache")
+    data_dir = "log"
     with SyncCrazyflie(get_address(), cf=cf) as scf:
-        init_log_conf(scf, logging_callback)
-        while i < 50:
-            print("Sleep")
-            time.sleep(0.5)
-            i += 1
+        init_log_conf(scf, logging_callback, data_dir)
+        while int(time.time()) < end:
+            pass
