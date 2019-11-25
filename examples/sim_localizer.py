6224875151b2d7210b547ab73bcfe081dd2d5ff2
Ali Benkassou <a.benkassou1@gmail.com>
2019-11-25 16:34:54 +0100

    Add argparse arguments for number of data points and particles

--- examples/sim_localizer.py
+++ examples/sim_localizer.py
@@ -1,23 +1,44 @@
-from crazyslam.localization import *
-from crazyslam.mapping import *
+import argparse
 from tqdm import tqdm
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from crazyslam.mapping import *
+from crazyslam.localization import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--n_data_points",
+    default=4,
+    help="Number of data points to use for each scan",
+)
+parser.add_argument(
+    "--n_particles",
+    default=100,
+    help="Number of particles in the particle filter",
+)
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+
     # Load data
-    data = loadmat("crazyslam/data/localization_data.mat")
+    data = loadmat("data/localization_data.mat")
     ranges = data["ranges"]
     angles = data["scanAngles"]
-    selected_idx = np.linspace(0, len(angles)-1, 100, dtype="int32")
+    selected_idx = np.linspace(
+        0,
+        len(angles)-1,
+        int(args.n_data_points),
+        dtype="int32"
+    )
     angles = angles[selected_idx, :]
     ranges = ranges[selected_idx, :]
     gt_map = data["M"].T - 0.5
 
     # Init useful variables
     count = 3700
-    n_particles = 300
+    n_particles = int(args.n_particles)
     params = init_params_dict(size=30, resolution=25, origin=(684, 571))
     particles = init_random_particles(n_particles)
     pose = np.zeros((3, count))
@@ -26,7 +47,7 @@ if __name__ == '__main__':
         [0, -1],
         [-1, 10],
     ])
-    resampling_threshold = 100
+    resampling_threshold = (n_particles * 10) // 100
 
     # Main loop
     for t in tqdm(range(count)):
@@ -45,5 +66,11 @@ if __name__ == '__main__':
     position = discretize(pose[:2, :], params)
     plt.figure()
     plt.imshow(gt_map, cmap="gray")
-    plt.plot(position[1, :], position[0, :], "bo", markersize=1, label="estimate")
+    plt.plot(
+        position[1, :],
+        position[0, :],
+        "bo",
+        markersize=1,
+        label="estimate"
+    )
     plt.show()
