49677611ede4bfb740f8b2626baace3cc977f8a5
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-12-10 16:48:54 +0100

    Add docstrings

--- crazyslam/slam.py
+++ crazyslam/slam.py
@@ -1,6 +1,7 @@
 """SLAM module
 
-This module implements ...
+This module implements a SLAM algorithm to correct noisy motion updates and
+state estimates while also mapping the environment.
 """
 
 
@@ -11,6 +12,20 @@ from crazyslam.localization import get_state_estimate
 
 class SLAM():
     """
+    SLAM agent. Initialized at the beginning of the flight.
+
+    The main goal of this class is to store all the useful variables for
+    the SLAM algorithm.
+
+    Attributes:
+        map: Occupancy grid map
+        params: Grid map parameters dictionary
+        n_particles: Number of particles for the Particle Filter
+        system_noise_variance: Variance for noise generation
+        correlation_matrix: Matrix for computing the correlation scores
+        resampling_threshold: Threshold for resampling
+        current_state: Current state (i.e. particle with the highest score)
+        particles: Set of state estimates and their corresponding weight
     """
 
     def __init__(
@@ -22,6 +37,10 @@ class SLAM():
         correlation_matrix,
     ):
         """
+        Initialize a SLAM agent.
+
+        Store all arguments and initialize the particles with current_state
+        as a first state estimate.
         """
         self.map = create_empty_map(params)
         self.params = params
@@ -37,7 +56,18 @@ class SLAM():
 
     def update_state(self, ranges, angles, motion_update):
         """
+        Update state estimate. One iteration of the SLAM algorithm
+
+        Args:
+            ranges: Set on range inputs from sensor
+            angles: Scan angles
+            motion_update: Update to apply to the current state
+
+        Returns:
+            Updated state estimate
+
         """
+        # map update
         self.map = update_grid_map(
             self.map,
             ranges,
@@ -50,6 +80,7 @@ class SLAM():
         self.particles[:3, :] += motion_update.reshape((3, 1)) \
             * np.ones((3, self.n_particles))
 
+        # state update
         self.current_state, self.particles = get_state_estimate(
             self.particles,
             self.system_noise_variance,
@@ -60,5 +91,4 @@ class SLAM():
             angles,
             self.resampling_threshold
         )
-
         return self.current_state
